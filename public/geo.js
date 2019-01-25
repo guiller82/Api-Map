49d3f7cd7bbf4bf0c8738875c96b9d04cb0ee77f
Guillermo Salazar <info@guillermosalazar.net>
2019-01-25 16:41:56 -0500

    Two First service

--- public/geo.js
+++ public/geo.js
@@ -1,5 +1,11 @@
 //geocoder = new google.maps.Geocoder();
 var geocoder;
+var vartokens = 0;
+//alert(vartokens);
+var minus = 0;
+
+
+
 
 function initialize() {
   geocoder = new google.maps.Geocoder();
@@ -16,6 +22,11 @@ console.log ('hola');*/
 //function codeAddress(givenAddress) {
 
 function codeAddress() { 
+
+  
+vartokens = document.getElementById("token").value;
+//console.log (vartokens);
+  var loc=[];
   //var address = givenAddress;
  // alert(type(givenAddress));
    // var address = document.getElementById("address").value;
@@ -23,7 +34,26 @@ function codeAddress() {
   var address = document.getElementById("address").value;
   //console.log(address);
     geocoder.geocode( { 'address': address }, function(r, s) {
-      alert(r[0].geometry.location);
-      //console.log((r[0].geometry.location));
+     // alert (r[0].geometry.location);
+     //var geocode = (r[0].geometry.location);
+     loc[0]=r[0].geometry.location.lat();
+     loc[1]=r[0].geometry.location.lng();
+    
+
+   
+     maincontent =  loc[0] + "," + loc[1] ;
+     document.getElementById("geoCode").innerText = maincontent;
+    // console.log = vartokens;
+     document.getElementById("token").value = showTokens(vartokens);
+     
     });
-  }
+  };
+
+
+function showTokens (tokensi){
+
+  tokensi = tokensi - 1;
+return tokensi;
+
+}
+
